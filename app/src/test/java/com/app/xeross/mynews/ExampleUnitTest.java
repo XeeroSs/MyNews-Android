b4e2a243a7a2a6b72646a0ba61b710818ead3b19
XeeroSs <qmasini@gmail.com>
2019-03-07 20:21:30 +0100

    CircleCI

--- app/src/test/java/com/app/xeross/mynews/ExampleUnitTest.java
+++ app/src/test/java/com/app/xeross/mynews/ExampleUnitTest.java
@@ -1,20 +1,6 @@
 package com.app.xeross.mynews;
 
-import android.util.Log;
-
-import com.app.xeross.mynews.Model.Articles.ArticlesTop;
-import com.app.xeross.mynews.Model.Utils.ApiClient;
-import com.app.xeross.mynews.Model.Utils.ApiInterface;
-
-import org.junit.Assert;
 import org.junit.Test;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
-import static com.app.xeross.mynews.Model.Utils.Constants.API_KEY;
-import static org.junit.Assert.*;
 
 /**
  * ApiTopStories local unit test, which will execute on the development machine (host).
@@ -24,19 +10,5 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void testArticle() throws Exception {
-        ApiInterface apiInterface = ApiClient.getClient().create(ApiInterface.class);
-        Call<ArticlesTop> calls = apiInterface.getTopStories("movies", API_KEY);
-
-        calls.enqueue(new Callback<ArticlesTop>() {
-            @Override
-            public void onResponse(Call<ArticlesTop> call, Response<ArticlesTop> response) {
-                    assertEquals(20, 31);
-            }
-
-            @Override
-            public void onFailure(Call<ArticlesTop> call, Throwable t) {
-                Log.d("TAG", "Response = " + t.toString());
-            }
-        });
     }
 }
